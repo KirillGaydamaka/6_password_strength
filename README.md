3b4388be715f8505e708d335ded2928f27197587
KirillGaydamaka <k.gaydamaka@gmail.com>
2019-10-05 13:12:23 +0300

    Basic solution

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+The script calculates password strength. It takes passoword from user input.
+User info and password blacklist are set as global variables.
+
+The script needs Python 3.5 interpreter.
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+$ python password_strength.py
+```
+
+Example of script work:
+
+```bash
+Input password: A12DF4#$
+Password complexity:  9
+
+Process finished with exit code 0
+```
+
+
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,84 @@
+import re
 
 
 def get_password_strength(password):
-    pass
+    checklist = []
+
+    # length
+    if len(password) > 5:
+        checklist.append(1)
+    else:
+        checklist.append(0)
+
+    # the use of both upper-case and lower-case letters (case sensitivity)
+    if re.search('[A-Z]', password) and re.search('[a-z]', password):
+        checklist.append(1)
+    else:
+        checklist.append(0)
+
+    # inclusion of one or more numerical digits
+    if re.search('[\d]', password):
+        checklist.append(1)
+    else:
+        checklist.append(0)
+
+    # inclusion of special characters, such as @, #, $
+    if re.search('[@#$]', password):
+        checklist.append(1)
+    else:
+        checklist.append(0)
+
+    # prohibition of words found in a password blacklist
+    if not any(substring in password.lower() for substring in password_blacklist):
+        checklist.append(1)
+    else:
+        checklist.append(0)
+
+    # prohibition of words found in the user's personal information
+    words_list = user_personal_info.lower().split()
+    if not any(substring in password.lower() for substring in words_list):
+        checklist.append(1)
+    else:
+        checklist.append(0)
+
+    # prohibition of use of company name or an abbreviation
+    if not company.lower() in password:
+        checklist.append(1)
+    else:
+        checklist.append(0)
+
+    company_splitted = company.split()
+    if len(company_splitted) > 1:
+        abbreviation = ''.join(word[0] for word in company_splitted)
+        if not abbreviation.lower() in password:
+            checklist.append(1)
+        else:
+            checklist.append(0)
+
+    # prohibition of passwords that match the format of calendar dates,
+    # license plate numbers, telephone numbers, or other common numbers
+    if not re.fullmatch('\d\d/\d\d/\d{4}', password) \
+            or re.fullmatch('\d{4}/\d\d/\d\d', password) \
+            or re.fullmatch('\d\d-\d\d-\d{4}', password) \
+            or re.fullmatch('\d{4}-\d\d-\d\d', password) \
+            or re.search('\d{3}-\d\d-\d\d', password) \
+            or re.fullmatch('[a-zA-Z]\d{3}[a-zA-Z]{2}\d{2,3}', password):
+        checklist.append(1)
+    else:
+        checklist.append(0)
+
+    password_strength = round(10*sum(checklist)/len(checklist))
+
+    return password_strength
+
+
+def main():
+    password = input('Input password: ')
+    print('Password complexity: ', get_password_strength(password))
 
 
 if __name__ == '__main__':
-    pass
+    password_blacklist = ['password', 'pwd', 'parol']
+    user_personal_info = 'Ivanov Ivan Inanovich, born 21.12.1975 in Moscow'
+    company = 'General motors'
+    main()
